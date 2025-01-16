48fd528fa4c303c7cc4122ca05b49e6222b0cb8f
Warpstar10010001 <alpay.gueruez@hsrw.org>
2025-01-16 18:46:50 +0100

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -4,13 +4,18 @@ public class Calculator {
 
     public void exec(int selection){
         if(!Data.isAdmin()){
-            if(Data.getModeAvailability()[selection] == false){
-                Main.exit(true, 2);
+            if(selection <= (Data.getModeAvailability().length - 1)){
+                if(Data.getModeAvailability()[selection] == false){
+                    Main.exit(true, 2);
+                }
+            } else {
+                Main.exit(true, 1);
             }
         }
         switch (selection){
             case 1 -> BMI.calculate();
             case 2 -> BMR.calculate();
+            case 3 -> BFP.calculate();
             default -> Main.exit(true, 1);
         }
     }
