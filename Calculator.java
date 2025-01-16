07daa99b53bfb677bf64d843f521e77ca3b3969b
Warpstar10010001 <alpay.gueruez@hsrw.org>
2025-01-16 01:42:22 +0100

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,42 @@
 //Boo!!
 
+import java.util.Scanner;
+
 public class Calculator {
-    //Calculates all values and returns the result;
+    //Global objects & classes:
+    static Data data = new Data();
+    static Scanner scanner = new Scanner(System.in);
+
+    public void bmi(){
+        System.out.println("++Body-Mass-Index Calculator++\n");
+
+        if(!(data.isHeightAvailable() || data.isWeightAvailable())){
+            System.out.println("Please enter following data:\n");
+
+            if(!(data.isHeightAvailable())){
+                System.out.print("Height [cm]: ");
+                data.setHeight((scanner.nextDouble() / 100));
+                data.setHeightAvailable(true);
+            }
+
+            if (!data.isWeightAvailable()){
+                System.out.print("Weight [kg]: ");
+                data.setWeight(scanner.nextDouble());
+                data.setWeightAvailable(true);
+            }
+
+            System.out.println();
+        }
+
+        double result = data.getWeight() / Math.pow(data.getHeight(), 2);
+
+        System.out.printf("Result: %.1f%n", result);
+        System.out.println("Interpretation: " + bmiInterpretation(result));
+
+        Main.restart();
+    }
+
+    public String bmiInterpretation(double bmi){
+        return "not implemented yet";
+    }
 }
