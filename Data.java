e21b8ae809b1483a3ff08f34d18a1ed21c6db880
Warpstar10010001 <alpay.gueruez@hsrw.org>
2025-01-16 01:42:23 +0100

    Update Data.java

--- Data.java
+++ Data.java
@@ -1,5 +1,48 @@
 //Boo!!
 
 public class Data {
-    //Stores all data inputs made by the user
+    //Global variables:
+    private boolean weightAvailable;
+    private double weight;
+    private boolean heightAvailable;
+    private double height;
+
+    public void reset(){
+        //Somehow doesnt reset the availability, needs rework
+        this.weightAvailable = false;
+        this.heightAvailable = false;
+        //Add other booleans
+    }
+
+    public boolean isWeightAvailable() {
+        return weightAvailable;
+    }
+
+    public void setWeightAvailable(boolean weightAvailable) {
+        this.weightAvailable = weightAvailable;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public boolean isHeightAvailable() {
+        return heightAvailable;
+    }
+
+    public void setHeightAvailable(boolean heightAvailable) {
+        this.heightAvailable = heightAvailable;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
 }
